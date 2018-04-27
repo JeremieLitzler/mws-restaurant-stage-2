74568a39185dca3dfc541c23fc21f526eccbb548
Jeremie Litzler <jeremie.litzler@conduent.com>
2018-04-27 13:25:34 +0200

    #25: move the common methods

---
+++ assets/js/lib/helpers.js
@@ -0,0 +1,35 @@
+/**
+ * Restaurant page URL.
+ */
+function urlForRestaurant(restaurant) {
+  if (!restaurant) {
+    console.error(
+      "Restaurant is null. Cannot build the URL to information page"
+    );
+    return;
+  }
+
+  return `./restaurant.html?id=${restaurant.id}`;
+}
+/**
+ * Restaurant image URL
+ * @param {Object} restaurant
+ * @param {int|null} imgWidth
+ * @param {bool} usePlaceholder
+ */
+function imageUrlForRestaurant(
+  restaurant,
+  imgWidth = 800,
+  usePlaceholder = false
+) {
+  if (!restaurant) {
+    console.error("Restaurant is null. Cannot build the image URL");
+    return;
+  }
+  const imgFolderPath = "./img/";
+  if (usePlaceholder) {
+    return `./img/img-ph-${imgWidth}w.svg`;
+  }
+
+  return `${imgFolderPath}${restaurant.id}-${imgWidth}w.jpg`;
+}
