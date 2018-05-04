bcee03f432fe112c3bf1b97555a291446e461730
Jeremie Litzler <jeremie.litzler@conduent.com>
2018-05-04 14:42:53 +0200

    #33-feat: try to start to replace callbacks

--- js/db/fetch.restaurants.js
+++ js/db/fetch.restaurants.js
@@ -1,3 +1,5 @@
+import { resolve } from "dns";
+
 /**
  * Database URL.
  * Change this to your server's.
@@ -10,75 +12,94 @@ const STATIC_DATA_URL = `${location.origin}/data/restaurants.json`;
  * @param {*} callback
  */
 function fetchRestaurants(callback) {
-    fetchCache(callback);
+  fetchCache(callback);
+}
+
+function fetchRestaurantsWithPromise() {
+  return fetchCachePromise();
 }
 
 function fetchCache(callback) {
-    idbKeyval
-        .keys()
-        .then(response => {
-            console.log("idb keys", response);
-            const items = [];
-            response.forEach(key => {
-                getCacheItem(key)
-                    .then(value => {
-                        items.push(value);
-                    })
-                    .catch(err => {
-                        console.error("GetCacheItem failed", err);
-                    });
-            });
-            //still fetch the API in the background to update the cache.
-            fetchApi(callback);
+  idbKeyval
+    .keys()
+    .then(response => {
+      console.log("idb keys", response);
+      const items = [];
+      response.forEach(key => {
+        getCacheItem(key)
+          .then(value => {
+            items.push(value);
+          })
+          .catch(err => {
+            console.error("GetCacheItem failed", err);
+          });
+      });
+      //still fetch the API in the background to update the cache.
+      fetchApi(callback);
 
-            //and return the cache items immediatly
-            callback(null, items);
-        })
-        .catch(err => {
-            callback(err, null);
-        });
+      //and return the cache items immediatly
+      callback(null, items);
+    })
+    .catch(err => {
+      callback(err, null);
+    });
 }
 
+function fetchCachePromise() {
+  return idbKeyval
+    .keys()
+    .then(data => {
+      return parseData(data);
+    })
+    .catch(err => {
+      console.error(err, null);
+      return false;
+    });
+}
+
+function parseData(data) {
+  return data;
+}
 /**
  * Fetch the data at DATABASE_URL using the Web API method Fetch
  * @param {*} callback
  */
 function fetchApi(callback) {
-    fetch(API_URL)
+  fetch(API_URL)
+    .then(response => {
+      if (response.ok) {
+        const jsonData = response.json();
+        //console.log(jsonData);
+        return jsonData;
+      }
+      console.log("Fetch failed response", response);
+    })
+    .then(restaurants => {
+      cacheItems(restaurants);
+      callback(null, restaurants);
+    })
+    .catch(err => {
+      console.error(
+        "API is not available. Falling back to the static data...",
+        err
+      );
+      fetch(STATIC_DATA_URL)
         .then(response => {
-            if (response.ok) {
-                const jsonData = response.json();
-                //console.log(jsonData);
-                return jsonData;
-            }
-            console.log("Fetch failed response", response);
+          if (!response.ok) {
+            return callback(response, null);
+          }
+          const jsonData = response.json();
+          //console.log(jsonData);
+          return jsonData;
         })
-        .then(restaurants => {
-            cacheItems(restaurants);
-            callback(null, restaurants);
+        .then(data => {
+          const restaurants = Object.values(data.restaurants);
+          cacheItems(restaurants);
+          callback(null, restaurants);
         })
         .catch(err => {
-            console.error(
-                "API is not available. Falling back to the static data...",
-                err
-            );
-            fetch(STATIC_DATA_URL)
-                .then(response => {
-                    if (!response.ok) {
-                        return callback(response, null);
-                    }
-                    const jsonData = response.json();
-                    //console.log(jsonData);
-                    return jsonData;
-                })
-                .then(data => {
-                    const restaurants = Object.values(data.restaurants);
-                    cacheItems(restaurants);
-                    callback(null, restaurants);
-                })
-                .catch(err => {
-                    console.error("Some error appended", err);
-                    callback(err, null);
-                });
+          console.error("Some error appended", err);
+          callback(err, null);
         });
+    });
 }
