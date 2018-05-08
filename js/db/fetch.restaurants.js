cf2f127f1c08571290375f0622298aeb819e2e90
Jérémie Litzler <contact@jeremielitzler.net>
2018-05-08 18:41:10 +0200

    #33-feat: fetch the cuisines and neighborhoods in single function

--- js/db/fetch.restaurants.js
+++ js/db/fetch.restaurants.js
@@ -10,109 +10,120 @@ const STATIC_DATA_URL = `${location.origin}/data/restaurants.json`;
  * @param {*} callback
  */
 function fetchRestaurants(callback) {
-  fetchCache(callback);
+    fetchCache(callback);
 }
 
 function fetchRestaurantsWithPromise() {
-  return fetchCachePromise();
+    return fetchCachePromise();
 }
 
 function fetchCache(callback) {
-  idbKeyval
-    .keys()
-    .then(response => {
-      console.log("idb keys", response);
-      const items = [];
-      response.forEach(key => {
-        getCacheItem(key)
-          .then(value => {
-            items.push(value);
-          })
-          .catch(err => {
-            console.error("GetCacheItem failed", err);
-          });
-      });
-      //still fetch the API in the background to update the cache.
-      fetchApi(callback);
+    idbKeyval
+        .keys()
+        .then(response => {
+            console.log("idb keys", response);
+            const items = [];
+            response.forEach(key => {
+                getCacheItem(key)
+                    .then(value => {
+                        items.push(value);
+                    })
+                    .catch(err => {
+                        console.error("GetCacheItem failed", err);
+                    });
+            });
+            //still fetch the API in the background to update the cache.
+            fetchApi(callback);
 
-      //and return the cache items immediatly
-      callback(null, items);
-    })
-    .catch(err => {
-      callback(err, null);
-    });
+            //and return the cache items immediatly
+            callback(null, items);
+        })
+        .catch(err => {
+            callback(err, null);
+        });
 }
 
 function fetchCachePromise() {
-  return idbKeyval
-    .keys()
-    .then(data => {
-      return parseData(data);
-    })
-    .catch(err => {
-      console.error(err, null);
-      return false;
-    });
+    return idbKeyval
+        .keys()
+        .then(data => {
+            return parseData(data);
+        })
+        .catch(err => {
+            console.error(err, null);
+            return false;
+        });
 }
 
 function parseData(data) {
-  var allGetItemPromises = [];
-  data.forEach(key => {
-    allGetItemPromises.push(
-      getCacheItem(key)
-        .then(value => {
-          return value;
-        })
-        .catch(err => {
-          console.error("GetCacheItem failed", err);
-        })
-    );
-  });
-  console.log(allGetItemPromises);
-  return Promise.all(allGetItemPromises).then(fullItems => {
-    return fullItems;
-  });
+    var allGetItemPromises = [];
+    data.forEach(key => {
+        allGetItemPromises.push(
+            getCacheItem(key)
+                .then(value => {
+                    return value;
+                })
+                .catch(err => {
+                    console.error("GetCacheItem failed", err);
+                })
+        );
+    });
+    console.log(allGetItemPromises);
+    return Promise.all(allGetItemPromises).then(fullItems => {
+        if (!fullItems) {
+            return false;
+        }
+        if (
+            fullItems === undefined ||
+            fullItems === null ||
+            fullItems.length === 0
+        ) {
+            return false;
+        }
+
+        return fullItems;
+    });
 }
 /**
  * Fetch the data at DATABASE_URL using the Web API method Fetch
  * @param {*} callback
  */
 function fetchApi(callback) {
-  fetch(API_URL)
-    .then(response => {
-      if (response.ok) {
-        const jsonData = response.json();
-        //console.log(jsonData);
-        return jsonData;
-      }
-      console.log("Fetch failed response", response);
-    })
-    .then(restaurants => {
-      cacheItems(restaurants);
-      callback(null, restaurants);
-    })
-    .catch(err => {
-      console.error(
-        "API is not available. Falling back to the static data...",
-        err
-      );
-      fetch(STATIC_DATA_URL)
+    fetch(API_URL)
         .then(response => {
-          if (!response.ok) {
-            return callback(response, null);
-          }
-          const jsonData = response.json();
-          //console.log(jsonData);
-          return jsonData;
+            if (response.ok) {
+                const jsonData = response.json();
+                //console.log(jsonData);
+                return jsonData;
+            }
+            console.log("Fetch failed response", response);
         })
-        .then(data => {
-          const restaurants = Object.values(data.restaurants);
-          cacheItems(restaurants);
-          callback(null, restaurants);
+        .then(restaurants => {
+            cacheItems(restaurants);
+            callback(null, restaurants);
         })
         .catch(err => {
-          console.error("Some error appended", err);
-          callback(err, null);
+            console.error(
+                "API is not available. Falling back to the static data...",
+                err
+            );
+            fetch(STATIC_DATA_URL)
+                .then(response => {
+                    if (!response.ok) {
+                        return callback(response, null);
+                    }
+                    const jsonData = response.json();
+                    //console.log(jsonData);
+                    return jsonData;
+                })
+                .then(data => {
+                    const restaurants = Object.values(data.restaurants);
+                    cacheItems(restaurants);
+                    callback(null, restaurants);
+                })
+                .catch(err => {
+                    console.error("Some error appended", err);
+                    callback(err, null);
+                });
         });
-    });
 }
--- js/db/filters.js
+++ js/db/filters.js
@@ -2,234 +2,208 @@
  * Fetch restaurants by a cuisine type with proper error handling.
  */
 function fetchRestaurantByCuisine(cuisine, callback) {
-  // Fetch all restaurants  with proper error handling
-  fetchRestaurants((error, restaurants) => {
-    if (error) {
-      callback(error, null);
-    }
-    if (restaurants === undefined || restaurants === null) {
-      return;
-    }
-    // Filter restaurants to have only given cuisine type
-    const results = restaurants.filter(r => r.cuisine_type == cuisine);
-    callback(null, results);
-  });
+    // Fetch all restaurants  with proper error handling
+    fetchRestaurants((error, restaurants) => {
+        if (error) {
+            callback(error, null);
+        }
+        if (restaurants === undefined || restaurants === null) {
+            return;
+        }
+        // Filter restaurants to have only given cuisine type
+        const results = restaurants.filter(r => r.cuisine_type == cuisine);
+        callback(null, results);
+    });
 }
 /**
  * Fetch restaurants by a neighborhood with proper error handling.
  */
 function fetchRestaurantByNeighborhood(neighborhood, callback) {
-  // Fetch all restaurants
-  fetchRestaurants((error, restaurants) => {
-    if (error) {
-      callback(error, null);
-    }
-    if (restaurants === undefined || restaurants === null) {
-      return;
-    }
+    // Fetch all restaurants
+    fetchRestaurants((error, restaurants) => {
+        if (error) {
+            callback(error, null);
+        }
+        if (restaurants === undefined || restaurants === null) {
+            return;
+        }
 
-    // Filter restaurants to have only given neighborhood
-    const results = restaurants.filter(r => r.neighborhood == neighborhood);
-    callback(null, results);
-  });
+        // Filter restaurants to have only given neighborhood
+        const results = restaurants.filter(r => r.neighborhood == neighborhood);
+        callback(null, results);
+    });
 }
 /**
  * Fetch restaurants by a cuisine type with proper error handling.
  */
 function fetchRestaurantByCuisineWithPromise(cuisine) {
-  return fetchRestaurants()
-    .then(result => {
-      if (!result) {
-        return result;
-      }
+    return fetchRestaurants()
+        .then(result => {
+            if (!result) {
+                return result;
+            }
 
-      // Filter restaurants to have only given cuisine type
-      const results = restaurants.filter(r => r.cuisine_type == cuisine);
-      return results;
-    })
-    .catch(err => {
-      console.error("Error in fetchRestaurantByCuisine", err);
-    });
+            // Filter restaurants to have only given cuisine type
+            const results = restaurants.filter(r => r.cuisine_type == cuisine);
+            return results;
+        })
+        .catch(err => {
+            console.error("Error in fetchRestaurantByCuisine", err);
+        });
 }
 /**
  * Fetch restaurants by a neighborhood with proper error handling.
  */
 function fetchRestaurantByNeighborhoodWithPromise(neighborhood) {
-  return fetchRestaurants()
-    .then(result => {
-      if (!result) {
-        return result;
-      }
+    return fetchRestaurants()
+        .then(result => {
+            if (!result) {
+                return result;
+            }
 
-      // Filter restaurants to have only given cuisine type
-      const results = restaurants.filter(r => r.neighborhood == neighborhood);
-      return results;
-    })
-    .catch(err => {
-      console.error("Error in fetchRestaurantByNeighborhood", err);
-    });
+            // Filter restaurants to have only given cuisine type
+            const results = restaurants.filter(
+                r => r.neighborhood == neighborhood
+            );
+            return results;
+        })
+        .catch(err => {
+            console.error("Error in fetchRestaurantByNeighborhood", err);
+        });
 }
 /**
  * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
  */
 function fetchRestaurantByCuisineAndNeighborhood(
-  cuisine,
-  neighborhood,
-  callback
+    cuisine,
+    neighborhood,
+    callback
 ) {
-  // Fetch all restaurants
-  fetchRestaurants((error, restaurants) => {
-    if (error) {
-      callback(error, null);
-    }
-    if (restaurants === undefined || restaurants === null) {
-      return;
-    }
+    // Fetch all restaurants
+    fetchRestaurants((error, restaurants) => {
+        if (error) {
+            callback(error, null);
+        }
+        if (restaurants === undefined || restaurants === null) {
+            return;
+        }
 
-    let results = restaurants;
-    if (cuisine != "all") {
-      // filter by cuisine
-      results = results.filter(r => r.cuisine_type == cuisine);
-    }
-    if (neighborhood != "all") {
-      // filter by neighborhood
-      results = results.filter(r => r.neighborhood == neighborhood);
-    }
-    callback(null, results);
-  });
+        let results = restaurants;
+        if (cuisine != "all") {
+            // filter by cuisine
+            results = results.filter(r => r.cuisine_type == cuisine);
+        }
+        if (neighborhood != "all") {
+            // filter by neighborhood
+            results = results.filter(r => r.neighborhood == neighborhood);
+        }
+        callback(null, results);
+    });
 }
 /**
  * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
  */
 function fetchRestaurantFiltered(cuisine, neighborhood) {
-  // Fetch all restaurants
-  fetchRestaurants()
-    .then(restaurants => {
-      if (!restaurants) {
-        return false;
-      }
-      if (restaurants === undefined || restaurants === null) {
-        return;
-      }
+    // Fetch all restaurants
+    fetchRestaurants()
+        .then(restaurants => {
+            if (!restaurants) {
+                return false;
+            }
+            if (restaurants === undefined || restaurants === null) {
+                return;
+            }
 
-      let results = restaurants;
-      if (cuisine != "all") {
-        // filter by cuisine
-        results = results.filter(r => r.cuisine_type == cuisine);
-      }
-      if (neighborhood != "all") {
-        // filter by neighborhood
-        results = results.filter(r => r.neighborhood == neighborhood);
-      }
-      return results;
-    })
-    .catch(err => {
-      console.error(err);
-    });
+            let results = restaurants;
+            if (cuisine != "all") {
+                // filter by cuisine
+                results = results.filter(r => r.cuisine_type == cuisine);
+            }
+            if (neighborhood != "all") {
+                // filter by neighborhood
+                results = results.filter(r => r.neighborhood == neighborhood);
+            }
+            return results;
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
 
 /**
  * Fetch all neighborhoods with proper error handling.
  */
 function fetchNeighborhoods(callback) {
-  // Fetch all restaurants
-  fetchRestaurants((error, restaurants) => {
-    if (error) {
-      callback(error, null);
-    }
-    if (restaurants === undefined || restaurants === null) {
-      return;
-    }
+    // Fetch all restaurants
+    fetchRestaurants((error, restaurants) => {
+        if (error) {
+            callback(error, null);
+        }
+        if (restaurants === undefined || restaurants === null) {
+            return;
+        }
 
-    // Get all neighborhoods from all restaurants
-    const neighborhoods = restaurants.map(
-      (v, i) => restaurants[i].neighborhood
-    );
-    // Remove duplicates from neighborhoods
-    const uniqueNeighborhoods = neighborhoods.filter(
-      (v, i) => neighborhoods.indexOf(v) == i
-    );
-    callback(null, uniqueNeighborhoods, new IndexPage());
-  });
-}
-/**
- * Fetch all neighborhoods with proper error handling.
- */
-function getNeighborhoodsWithPromise() {
-  // Fetch all restaurants
-  return fetchRestaurantsWithPromise()
-    .then(restaurants => {
-      if (!restaurants) {
-        return false;
-      }
-      if (
-        restaurants === undefined ||
-        restaurants === null ||
-        restaurants.length === 0
-      ) {
-        return false;
-      }
-
-      // Get all neighborhoods from all restaurants
-      const neighborhoods = restaurants.map(
-        (v, i) => restaurants[i].neighborhood
-      );
-      // Remove duplicates from neighborhoods
-      const uniqueNeighborhoods = neighborhoods.filter(
-        (v, i) => neighborhoods.indexOf(v) == i
-      );
-      return uniqueNeighborhoods;
-    })
-    .catch(err => {
-      console.error(err);
+        // Get all neighborhoods from all restaurants
+        const neighborhoods = restaurants.map(
+            (v, i) => restaurants[i].neighborhood
+        );
+        // Remove duplicates from neighborhoods
+        const uniqueNeighborhoods = neighborhoods.filter(
+            (v, i) => neighborhoods.indexOf(v) == i
+        );
+        callback(null, uniqueNeighborhoods, new IndexPage());
     });
 }
+
 /**
  * Fetch all cuisines with proper error handling.
  */
 function fetchCuisines(callback) {
-  // Fetch all restaurants
-  fetchRestaurants((error, restaurants) => {
-    if (error) {
-      callback(error, null);
-    }
-    if (restaurants === undefined || restaurants === null) {
-      return;
-    }
+    // Fetch all restaurants
+    fetchRestaurants((error, restaurants) => {
+        if (error) {
+            callback(error, null);
+        }
+        if (restaurants === undefined || restaurants === null) {
+            return;
+        }
 
-    // Get all cuisines from all restaurants
-    const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
-    // Remove duplicates from cuisines
-    const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i);
-    callback(null, uniqueCuisines, new IndexPage());
-    if (restaurants === undefined || restaurants === null) {
-      return;
-    }
-  });
-}
-/**
- * Fetch all cuisines with proper error handling.
- */
-function fetchCuisineswithPromise() {
-  // Fetch all restaurants
-  fetchRestaurants()
-    .then(restaurants => {
-      if (!restaurants) {
-        return false;
-      }
-      if (restaurants === undefined || restaurants === null) {
-        return;
-      }
-
-      // Get all cuisines from all restaurants
-      const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
-      // Remove duplicates from cuisines
-      const uniqueCuisines = cuisines.filter(
-        (v, i) => cuisines.indexOf(v) == i
-      );
-      return uniqueCuisines;
-    })
-    .catch(err => {
-      console.error(err);
+        // Get all cuisines from all restaurants
+        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
+        // Remove duplicates from cuisines
+        const uniqueCuisines = cuisines.filter(
+            (v, i) => cuisines.indexOf(v) == i
+        );
+        callback(null, uniqueCuisines, new IndexPage());
+        if (restaurants === undefined || restaurants === null) {
+            return;
+        }
     });
+}
+
+/**
+ * Fetch all neighborhoods with proper error handling.
+ */
+function getValuesFor(type) {
+    if (type !== "cuisine" && type !== "neighborhood") {
+        throw "type parameter must be cuisine or neighborhood";
+    }
+    // Fetch all restaurants
+    return fetchRestaurantsWithPromise()
+        .then(restaurants => {
+            let values = [];
+            if (type === "cuisine") {
+                values = restaurants.map((v, i) => restaurants[i].cuisine_type);
+            }
+            if (type === "neighborhood") {
+                values = restaurants.map((v, i) => restaurants[i].neighborhood);
+            }
+            const uniqueValues = values.filter(
+                (v, i) => values.indexOf(v) == i
+            );
+            return uniqueValues;
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
