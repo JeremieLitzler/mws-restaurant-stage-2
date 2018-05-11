6368895c7bdd867273a609d815c674e7b1624829
Jeremie Litzler <jeremie.litzler@conduent.com>
2018-05-11 13:41:24 +0200

    #33-refactor: implement promise-based fetching of the restaurant

--- js/db/fetch.restaurant.byid.js
+++ js/db/fetch.restaurant.byid.js
@@ -8,6 +8,23 @@ function fetchRestaurantByIdInCache(id, callback) {
       callback(err, null);
     });
 }
+
+function fetchRestaurant(id) {
+  return getCacheItem(id)
+    .then(restaurant => {
+      if (restaurant === undefined || restaurant === null) {
+        var apiPromise = fetchRestaurantFromApi(id);
+        return Promise.all(apiPromise).then(apiResponse => {
+          return apiPromise; //already cached in fetchRestaurantFromApi
+        });
+      }
+      fetchRestaurantFromApi(id); //call the api that will update the cache db.
+      return restaurant;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
 /**
  * Fetch a restaurant by its ID from the API.
  */
@@ -66,5 +83,41 @@ function fetchRestaurantByIdInApi(id, callback) {
           console.error("Some error appended", err);
           callback(err, null);
         });
+    });
+}
+/**
+ * Fetch a restaurant by its ID from the API.
+ */
+function fetchRestaurantFromApi(id) {
+  const DATABASE_URL = `http://localhost:1337/restaurants/${id}`;
+  fetch(DATABASE_URL)
+    .then(function(response) {
+      if (response.ok) {
+        const jsonData = response.json();
+        console.log(jsonData);
+        return jsonData;
+      }
+      if (DEBUG) console.log("Fetch failed response", response);
+    })
+    .then(restaurant => {
+      cacheItem(restaurant)
+        .then(response => {
+          if (DEBUG)
+            console.log(
+              `Just updated restaurant ID ${restaurant.id}`,
+              response
+            );
+          return restaurant;
+        })
+        .catch(err => {
+          console.error(
+            `Failed to cache the restaurant ID ${restaurant.id}in cache`,
+            err
+          );
+          return false;
+        });
+    })
+    .catch(err => {
+      return "Visit the home page!";
     });
 }
