90bf8fc61103380c5e7fcecac4f4d04bbe858302
Jeremie Litzler <jeremie.litzler@conduent.com>
2018-05-14 14:13:15 +0200

    #12-feat: load static but dynamic gmaps image

--- gulpfile.js
+++ gulpfile.js
@@ -12,67 +12,67 @@ const babel = require("gulp-babel");
  */
 const eslint = require("gulp-eslint");
 gulp.task("js-lint", () => {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
-    return (
-        gulp
-            .src([
-                "./js/*.js",
-                "!node_modules/**",
-                "!js/lazysizes**",
-                "!build/js/io.min.js"
-            ])
-            // eslint() attaches the lint output to the "eslint" property
-            // of the file object so it can be used by other modules.
-            .pipe(eslint())
-            // eslint.format() outputs the lint results to the console.
-            // Alternatively use eslint.formatEach() (see Docs).
-            .pipe(eslint.format())
-            // To have the process exit with an error code (1) on
-            // lint error, return the stream and pipe to failAfterError last.
-            .pipe(eslint.failAfterError())
-    );
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return (
+    gulp
+      .src([
+        "./js/*.js",
+        "!node_modules/**",
+        "!js/lazysizes**",
+        "!build/js/io.min.js"
+      ])
+      // eslint() attaches the lint output to the "eslint" property
+      // of the file object so it can be used by other modules.
+      .pipe(eslint())
+      // eslint.format() outputs the lint results to the console.
+      // Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.format())
+      // To have the process exit with an error code (1) on
+      // lint error, return the stream and pipe to failAfterError last.
+      .pipe(eslint.failAfterError())
+  );
 });
 
 /**
  * Copy the any files at the root of the project into the root of build folder
  */
 gulp.task("copy-root", () => {
-    gulp
-        .src([
-            "./sw.js",
-            "./favicon.ico",
-            "./site.webmanifest",
-            "./browserconfig.xml"
-        ])
-        .pipe(gulp.dest("./build"));
+  gulp
+    .src([
+      "./sw.js",
+      "./favicon.ico",
+      "./site.webmanifest",
+      "./browserconfig.xml"
+    ])
+    .pipe(gulp.dest("./build"));
 });
 /**
  * Copy the html source files into the root of build folder
  */
 gulp.task("copy-html", () => {
-    gulp
-        .src(["./index.html", "./restaurant.html", "./offline.html"])
-        .pipe(gulp.dest("./build"));
+  gulp
+    .src(["./index.html", "./restaurant.html", "./offline.html"])
+    .pipe(gulp.dest("./build"));
 });
 
 /**
  * Copy the icons into the build folder
  */
 gulp.task("copy-icons", () => {
-    gulp
-        .src(["./img/icons/*.svg", "./img/icons/*.png"])
-        .pipe(gulp.dest("./build/img/icons"));
+  gulp
+    .src(["./img/icons/*.svg", "./img/icons/*.png"])
+    .pipe(gulp.dest("./build/img/icons"));
 });
 
 /**
  * Copy the image placeholders into the build folder
  */
 gulp.task("copy-img-ph", () => {
-    //As long as the svg files are the image placeholders, this is fine.
-    gulp.src("./img/*.svg").pipe(gulp.dest("./build/img"));
+  //As long as the svg files are the image placeholders, this is fine.
+  gulp.src("./img/*.svg").pipe(gulp.dest("./build/img"));
 });
 
 /**
@@ -80,18 +80,18 @@ gulp.task("copy-img-ph", () => {
  */
 
 gulp.task("copy-static-data", () => {
-    gulp.src(["./data/restaurants.json"]).pipe(gulp.dest("./build/data"));
+  gulp.src(["./data/restaurants.json"]).pipe(gulp.dest("./build/data"));
 });
 
 /**
  * Run the copy tasks of the static assets into one task.
  */
 gulp.task("copy-static-assets", [
-    "copy-root",
-    "copy-html",
-    "copy-icons",
-    "copy-img-ph",
-    "copy-static-data"
+  "copy-root",
+  "copy-html",
+  "copy-icons",
+  "copy-img-ph",
+  "copy-static-data"
 ]);
 
 /**
@@ -99,33 +99,33 @@ gulp.task("copy-static-assets", [
  */
 const $ = require("gulp-load-plugins")();
 gulp.task("optim-images", () => {
-    return gulp
-        .src("img/*.{jpg,png}")
-        .pipe(
-            $.responsive(
-                {
-                    // Resize all JPG images to three different sizes: 200, 500, and 630 pixels
-                    "*.jpg": [
-                        { width: 64, rename: { suffix: "-64w" } },
-                        { width: 128, rename: { suffix: "-128w" } },
-                        { width: 360, rename: { suffix: "-360w" } },
-                        { width: 480, rename: { suffix: "-480w" } },
-                        {
-                            // Compress, strip metadata, and rename original image //used for the index.html across all viewports // //used for the index.html across all viewports
-                            rename: { suffix: "-800w" }
-                        }
-                    ]
-                },
-                {
-                    // Global configuration for all images
-                    // The output quality for JPEG, WebP and TIFF output formats
-                    quality: 70,
-                    progressive: true,
-                    withMetadata: false
-                }
-            )
-        ) // Use progressive (interlace) scan for JPEG and PNG output // Strip all metadata
-        .pipe(gulp.dest("build/img"));
+  return gulp
+    .src("img/*.{jpg,png}")
+    .pipe(
+      $.responsive(
+        {
+          // Resize all JPG images to three different sizes: 200, 500, and 630 pixels
+          "*.jpg": [
+            { width: 64, rename: { suffix: "-64w" } },
+            { width: 128, rename: { suffix: "-128w" } },
+            { width: 360, rename: { suffix: "-360w" } },
+            { width: 480, rename: { suffix: "-480w" } },
+            {
+              // Compress, strip metadata, and rename original image //used for the index.html across all viewports // //used for the index.html across all viewports
+              rename: { suffix: "-800w" }
+            }
+          ]
+        },
+        {
+          // Global configuration for all images
+          // The output quality for JPEG, WebP and TIFF output formats
+          quality: 70,
+          progressive: true,
+          withMetadata: false
+        }
+      )
+    ) // Use progressive (interlace) scan for JPEG and PNG output // Strip all metadata
+    .pipe(gulp.dest("build/img"));
 });
 
 /**
@@ -137,19 +137,19 @@ const pump = require("pump");
 const autoprefixer = require("gulp-autoprefixer");
 const uglifycss = require("gulp-uglifycss");
 gulp.task("optim-css", callback => {
-    pump(
-        [
-            gulp.src("css/**/*.css"),
-            autoprefixer(),
-            uglifycss({
-                maxLineLen: 80,
-                uglyComments: true
-            }),
-            concat("styles.css"),
-            gulp.dest("build/css")
-        ],
-        callback
-    );
+  pump(
+    [
+      gulp.src("css/**/*.css"),
+      autoprefixer(),
+      uglifycss({
+        maxLineLen: 80,
+        uglyComments: true
+      }),
+      concat("styles.css"),
+      gulp.dest("build/css")
+    ],
+    callback
+  );
 });
 
 /**
@@ -157,19 +157,19 @@ gulp.task("optim-css", callback => {
  */
 const critical = require("critical");
 gulp.task("critical-css", ["optim-css"], function() {
-    critical
-        .generate({
-            inline: true,
-            base: "./build",
-            src: "index.html",
-            dest: "index.html",
-            minify: true,
-            width: 320,
-            height: 480
-        })
-        .catch(err => {
-            console.error("gulp task critical failed", err);
-        });
+  critical
+    .generate({
+      inline: true,
+      base: "./build",
+      src: "index.html",
+      dest: "index.html",
+      minify: true,
+      width: 320,
+      height: 480
+    })
+    .catch(err => {
+      console.error("gulp task critical failed", err);
+    });
 });
 
 /**
@@ -182,40 +182,40 @@ let uglify = require("gulp-uglify-es").default;
  * jsCommonFiles is the list of javascript files that are common to both index and restaurant pages.
  */
 let jsCommonFiles = [
-    "./node_modules/idb-keyval/dist/idb-keyval-iife.js", //Idb with promise library
-    "./js/lib/debug.js", //Idb with promise library
-    "./js/lib/helpers.js",
-    "./js/db/cache.request.js",
-    "./js/app.js",
-    "./js/lazysizes.min.js",
-    "./js/gmaps/static.gmaps.js",
-    "./js/gmaps/lazyload.gmaps.js",
-    "./js/focus.handler.js"
+  "./node_modules/idb-keyval/dist/idb-keyval-iife.js", //Idb with promise library
+  "./js/lib/debug.js", //Idb with promise library
+  "./js/lib/helpers.js",
+  "./js/db/cache.request.js",
+  "./js/app.js",
+  "./js/lazysizes.min.js",
+  "./js/gmaps/static.gmaps.js",
+  "./js/gmaps/lazyload.gmaps.js",
+  "./js/focus.handler.js"
 ];
 
 let jsFilesIndexPage = [
-    "./js/db/fetch.restaurants.js",
-    "./js/db/filters.js",
-    "./js/select.change.handler.js",
-    "./js/main.js"
+  "./js/db/fetch.restaurants.js",
+  "./js/db/filters.js",
+  "./js/select.change.handler.js",
+  "./js/main.js"
 ];
 
 const finalJsFilesIndexPage = [...jsCommonFiles, ...jsFilesIndexPage];
 //console.log("JS files to optimise for index page", finalJsFilesIndexPage);
 gulp.task("optim-js-index-page", callback => {
-    pump(
-        [
-            gulp.src(finalJsFilesIndexPage),
-            sourcemaps.init(),
-            babel(),
-            concat("index.bundle.js"),
-            uglify(),
-            rename("index.bundle.min.js"),
-            sourcemaps.write(),
-            gulp.dest("build/js")
-        ],
-        callback
-    );
+  pump(
+    [
+      gulp.src(finalJsFilesIndexPage),
+      sourcemaps.init(),
+      babel(),
+      concat("index.bundle.js"),
+      uglify(),
+      rename("index.bundle.min.js"),
+      sourcemaps.write(),
+      gulp.dest("build/js")
+    ],
+    callback
+  );
 });
 
 /**
@@ -224,67 +224,67 @@ gulp.task("optim-js-index-page", callback => {
  *     - minifying the javascript code.
  */
 let jsFilesRestaurantPage = [
-    "./js/db/fetch.restaurant.byid.js",
-    "./js/restaurant_info.js"
+  "./js/db/fetch.restaurant.byid.js",
+  "./js/restaurant_info.js"
 ];
 const finalJsFilesRestaurantPage = [...jsCommonFiles, ...jsFilesRestaurantPage];
 //console.log("JS files to optimise for index page", finalJsFilesRestaurantPage);
 gulp.task("optim-js-restaurant-page", callback => {
-    pump(
-        [
-            gulp.src(finalJsFilesRestaurantPage),
-            sourcemaps.init(),
-            babel(),
-            concat("restaurant.bundle.js"),
-            uglify(),
-            rename("restaurant.bundle.min.js"),
-            sourcemaps.write(),
-            gulp.dest("build/js")
-        ],
-        callback
-    );
+  pump(
+    [
+      gulp.src(finalJsFilesRestaurantPage),
+      sourcemaps.init(),
+      babel(),
+      concat("restaurant.bundle.js"),
+      uglify(),
+      rename("restaurant.bundle.min.js"),
+      sourcemaps.write(),
+      gulp.dest("build/js")
+    ],
+    callback
+  );
 });
 
 /**
  * Minify the intersection observer library and put it the build folder
  */
 const intersectionObserverFile =
-    "./node_modules/intersection-observer/intersection-observer.js";
+  "./node_modules/intersection-observer/intersection-observer.js";
 //console.log("JS files to optimise for index page", finalJsFilesRestaurantPage);
 gulp.task("optim-js-io", callback => {
-    pump(
-        [
-            gulp.src(intersectionObserverFile),
-            sourcemaps.init(),
-            uglify(),
-            rename("io.min.js"),
-            sourcemaps.write(),
-            gulp.dest("build/js")
-        ],
-        callback
-    );
+  pump(
+    [
+      gulp.src(intersectionObserverFile),
+      sourcemaps.init(),
+      uglify(),
+      rename("io.min.js"),
+      sourcemaps.write(),
+      gulp.dest("build/js")
+    ],
+    callback
+  );
 });
 /**
  * Task to copy the javascript unoptimized in development.
  */
 gulp.task("copy-scripts", () => {
-    gulp
-        .src([
-            ...jsCommonFiles,
-            ...jsFilesIndexPage,
-            ...jsFilesRestaurantPage,
-            ...intersectionObserverFile
-        ])
-        .pipe(gulp.dest("./build"));
+  gulp
+    .src([
+      ...jsCommonFiles,
+      ...jsFilesIndexPage,
+      ...jsFilesRestaurantPage,
+      ...intersectionObserverFile
+    ])
+    .pipe(gulp.dest("./build"));
 });
 
 /**
  * Task to optimize the javascript scripts when deploying the application
  */
 gulp.task("scripts", [
-    "optim-js-index-page",
-    "optim-js-restaurant-page",
-    "optim-js-io"
+  "optim-js-index-page",
+  "optim-js-restaurant-page",
+  "optim-js-io"
 ]);
 /**
  * Default gulp task for development environnement that includes:
@@ -297,21 +297,21 @@ gulp.task("scripts", [
 //const compress = require("compression");
 //const browserSync = require("browser-sync").create();
 gulp.task(
-    "default",
-    [
-        //"js-lint",
-        "copy-static-assets",
-        "optim-images",
-        "optim-css",
-        //"critical-css",
-        "scripts"
-    ],
-    () => {
-        gulp.watch("./*.js", ["copy-root"]);
-        gulp.watch("./js/**/*.js", ["scripts"]);
-        gulp.watch("./css/**/*.css", ["optim-css"]);
-        gulp.watch("./**/*.html", ["copy-html"]);
-    }
+  "default",
+  [
+    //"js-lint",
+    "copy-static-assets",
+    "optim-images",
+    "optim-css",
+    //"critical-css",
+    "scripts"
+  ],
+  () => {
+    gulp.watch("./*.js", ["copy-root"]);
+    gulp.watch("./js/**/*.js", ["scripts"]);
+    gulp.watch("./css/**/*.css", ["optim-css"]);
+    gulp.watch("./**/*.html", ["copy-html"]);
+  }
 );
 
 /**
@@ -322,9 +322,9 @@ gulp.task(
  *     - optimize the javascript
  */
 gulp.task("go-live", [
-    //"js-lint",
-    "copy-static-assets",
-    "optim-images",
-    "critical-css",
-    "scripts"
+  //"js-lint",
+  "copy-static-assets",
+  "optim-images",
+  "critical-css",
+  "scripts"
 ]);
